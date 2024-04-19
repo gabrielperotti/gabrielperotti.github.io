b859746879a976a494be4975a50bec35a8ccb730
Gabriel Perotti <gabrielperotti@hotmail.com>
2024-04-19 19:19:02 -0300

    menu fix

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,9 @@
-(function($) {
+(function ($) {
   var toggle = document.getElementById("menu-toggle");
   var menu = document.getElementById("menu");
   var close = document.getElementById("menu-close");
 
-  toggle.addEventListener("click", function(e) {
+  toggle.addEventListener("click", function (e) {
     if (menu.classList.contains("open")) {
       menu.classList.remove("open");
     } else {
@@ -11,18 +11,20 @@
     }
   });
 
-  close.addEventListener("click", function(e) {
+  close.addEventListener("click", function (e) {
     menu.classList.remove("open");
   });
 
   // Close menu after click on smaller screens
-  $(window).on("resize", function() {
+  $(window).on("resize", checkWidth);
+  $(window).on("load", checkWidth);
+  function checkWidth() {
     if ($(window).width() < 846) {
-      $(".main-menu a").on("click", function() {
+      $(".main-menu a").on("click", function () {
         menu.classList.remove("open");
       });
     }
-  });
+  }
 
   $(".owl-carousel").owlCarousel({
     items: 4,
@@ -44,15 +46,15 @@
     }
   });
 
-  $(".hover").mouseleave(function() {
+  $(".hover").mouseleave(function () {
     $(this).removeClass("hover");
   });
 
-  $(".isotope-wrapper").each(function() {
+  $(".isotope-wrapper").each(function () {
     var $isotope = $(".isotope-box", this);
     var $filterCheckboxes = $('input[type="radio"]', this);
 
-    var filter = function() {
+    var filter = function () {
       var type = $filterCheckboxes.filter(":checked").data("type") || "*";
       if (type !== "*") {
         type = '[data-type="' + type + '"]';
